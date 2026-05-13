bd9834e57d78c1f0032603d26b93b869c20431bd
abhay032158-hue <abhay032158@gmail.com>
2026-05-13 15:42:22 +0530

    Refactor mouse event y-coordinate calculations

--- simpleChess/src/main/Mouse.java
+++ simpleChess/src/main/Mouse.java
@@ -9,7 +9,7 @@ public class Mouse extends MouseAdapter {
     public boolean pressed;
     public boolean pressedFired; 
     
-    // ➡️ Define a common offset based on the JFrame title bar height
+    // 
     private static final int Y_OFFSET = 30; // Starting with 30 pixels
 
     public Mouse(GamePanel gp) {
@@ -28,13 +28,13 @@ public class Mouse extends MouseAdapter {
     @Override
     public void mouseReleased(MouseEvent e) {
         x = e.getX();
-        y = e.getY() - Y_OFFSET; // Applying offset
+        y = e.getY() - Y_OFFSET;
         pressed = false;
     }
 
     @Override
     public void mouseDragged(MouseEvent e) {
         x = e.getX();
-        y = e.getY() - Y_OFFSET; //  Applying offset
+        y = e.getY() - Y_OFFSET;
     }
 }
